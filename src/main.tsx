522e389e60ef1bf59f4052e498b05c276bfcc695
Muhammad Yusuf <myusuf040599@gmail.com>
2023-05-07 17:15:13 +0700

    github pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/*" element={<App />}></Route>
-            </Routes>
-        </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <HashRouter>
+      <Routes>
+        <Route path="/*" element={<App />}></Route>
+      </Routes>
+    </HashRouter>
+  </React.StrictMode>
 )
